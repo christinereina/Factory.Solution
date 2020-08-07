28a640b01c4dd488fb45b4c3820e012488ae974a
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-07 10:08:10 -0700

    add machine model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.Engineers = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public string Description { get; set; }
+    public ICollection<EngineerMachine> Engineers { get; set;}
+  }
+}
