4919a81f900248c1f5c97004b8fd1eb868116f2a
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-07 10:19:19 -0700

    add homecontroller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,37 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Registrar.Models;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Factory.Solution.Models;
 
-namespace Factory.Solution.Controllers
+namespace Registrar.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      return View();
     }
+  }
 }
